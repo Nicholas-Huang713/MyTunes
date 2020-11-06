8d0bb19476c63ae6db03d3d84738114b890173c5
Nicholas_Huang713 <nhuang713@gmail.com>
2020-11-06 15:24:37 -0800

    music search started

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [songList, setSongList] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleKeyUp = (e) => {
+    e.preventDefault();
+    if(e.key === 'Enter') {
+        e.preventDefault();
+        const options = {
+          method: 'GET',
+          url: 'https://deezerdevs-deezer.p.rapidapi.com/search',
+          params: {q: inputText},
+          headers: {
+            'x-rapidapi-key': '97b3d67fd7msh8ae0214eedae588p157a2cjsn1de270448a3e',
+            'x-rapidapi-host': 'deezerdevs-deezer.p.rapidapi.com'
+          }
+        };
+        axios.request(options).then((response) => {
+          console.log(response.data);
+          setSongList(response.data);
+          setInputText('');
+        }).catch((error) => {
+          console.error(error);
+        })
+        
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Music Search
+      <input 
+        type="text"
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        placeholder="Search Artist"
+        onKeyUp={handleKeyUp}
+      />
     </div>
   );
 }
